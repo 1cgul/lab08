f30196caa5e639b0c12ad5505b11a9331f5a322b
1cgul <cgulino03@gmail.com>
2023-04-03 21:47:18 -0400

    cleaned up a line

--- src/main/java/com/mycompany/singlelinkedlist/SinglyLinkedListDemoApp.java
+++ src/main/java/com/mycompany/singlelinkedlist/SinglyLinkedListDemoApp.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.singlelinkedlist;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class SinglyLinkedListDemoApp {
