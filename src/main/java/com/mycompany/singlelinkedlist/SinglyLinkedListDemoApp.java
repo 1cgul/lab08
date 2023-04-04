61c87c1e6227593437ded1c2c8795006b0fe33e5
1cgul <cgulino03@gmail.com>
2023-04-03 21:30:36 -0400

    completed

--- src/main/java/com/mycompany/singlelinkedlist/SinglyLinkedListDemoApp.java
+++ src/main/java/com/mycompany/singlelinkedlist/SinglyLinkedListDemoApp.java
@@ -64,7 +64,8 @@ public class SinglyLinkedListDemoApp {
             }
          }
 
-      }System.out.println(primesWithThree.sum());
+      }
+      System.out.println(primesWithThree.sum());
 
 
    }
